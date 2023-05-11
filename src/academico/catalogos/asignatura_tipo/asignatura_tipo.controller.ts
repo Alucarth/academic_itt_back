6096be6456c60a08d807b5146101dae83a3badf5
dancastillo <dan.c4stillo@gmail.com>
2023-05-11 11:20:31 -0400

    asigantura tipo

--- src/academico/catalogos/asignatura_tipo/asignatura_tipo.controller.ts
+++ src/academico/catalogos/asignatura_tipo/asignatura_tipo.controller.ts
@@ -12,19 +12,25 @@ import {
   ValidationPipe,
   Delete,
 } from "@nestjs/common";
-import { ApiOperation } from "@nestjs/swagger";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "../../../auth/guards/jwt-auth.guard";
 import { AsignaturaTipoService } from "./asignatura_tipo.service";
 import { CreateAsignaturaTipoDto } from "./dto/createAsignaturaTipo.dto";
 
+@ApiTags("Crud Catalogo Asignatura Tipo")
 @Controller("asignatura-tipo")
 export class AsignaturaTipoController {
   constructor(private readonly asignaturaTipoService: AsignaturaTipoService) {}
 
-  @Post('')
+  @Get()
+  async getAll() {
+    return await this.asignaturaTipoService.getAll();
+  }
+
+  @Post("")
   @ApiOperation({
     summary: "Crea un asignatura",
-  }) 
+  })
   create(@Body() createAsignaturaTipoDto: CreateAsignaturaTipoDto) {
     return this.asignaturaTipoService.create(createAsignaturaTipoDto);
   }
