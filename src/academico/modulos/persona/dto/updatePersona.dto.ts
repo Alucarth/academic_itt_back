c73b66119a060fdfb7b5e04dc8913a420aacb10a
dancastillo <dan.c4stillo@gmail.com>
2023-05-15 11:39:23 -0400

    update persona I

---
+++ src/academico/modulos/persona/dto/updatePersona.dto.ts
@@ -0,0 +1,101 @@
+import { Optional } from '@nestjs/common';
+import { Transform } from 'class-transformer';
+import { IsBoolean, IsDateString, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class UpdatePersonaoDto {
+
+    @IsNotEmpty({ message: "el Numero de Carnet es requerido" })
+    @IsString()
+    @Transform(({ value }) => value?.trim())
+    carnetIdentidad: string;
+
+    @IsString()
+    complemento: string;
+    
+    @IsString()
+    paterno: string;
+
+    @IsNotEmpty({ message: "el Ap. Materno es requerido" })
+    @IsString()
+    materno: string;
+
+    @IsNotEmpty({ message: "el Nombre es requerido" })
+    @IsString()
+    nombre: string;
+
+    @IsDateString()
+    fechaNacimiento?: Date | null
+    
+    @IsNotEmpty({ message: "Genero es requerido" })
+    @IsNumber()
+    generoTipoId: number;
+    
+    @IsNotEmpty({ message: "Estado Civil es requerido" })
+    @IsNumber()
+    estadoCivilTipoId: number;
+
+    @IsNotEmpty({ message: "Sangre tipo es requerido" })
+    @IsNumber()
+    sangreTipoId: number;
+
+    @IsNotEmpty({ message: "Idioma materno es requerido" })
+    @IsNumber()
+    maternoIdiomaTipoId: number;
+
+    @IsNotEmpty({ message: "Segip  tipo es requerido" })
+    @IsNumber()
+    segipTipoId: number;
+
+    @IsNotEmpty({ message: "Lugar de expedicion es requerido" })
+    @IsNumber()
+    expedidoUnidadTerritorialId: number;
+
+    @IsNotEmpty({ message: "Lugar de nacimiento es requerido" })
+    @IsNumber()
+    nacimientoUnidadTerritorialId: number;
+
+    @IsString()
+    nacimientoOficialia: string;
+
+    @IsString()
+    nacimientoLibro: string;
+
+    @IsString()
+    nacimientoPartida: string;
+
+    @IsString()
+    nacimientoFolio: string;
+
+    @IsString()
+    carnetIbc: string;
+
+    @IsString()
+    pasaporte: string;
+
+    @IsString()
+    libretaMilitar: string;
+
+    @IsBoolean()
+    dobleNacionalidad: boolean;
+
+    @IsString()
+    codigoRda: string;
+
+    @IsString()
+    nacimientoLocalidad: string;
+
+    @IsBoolean()
+    tieneDiscapacidad: boolean;
+   
+    @IsString()
+    telefono: string;
+
+    @IsString()
+    email: string;
+   
+    @IsNotEmpty()
+    @IsNumber()
+    ciExpedidoTipoId: number;
+
+
+}
