62f4a922685edcb6b7b2d0df8cf50da57b2486be
dancastillo <dan.c4stillo@gmail.com>
2023-05-19 11:28:17 -0400

    getAllDetalleInscripcionPersona

--- src/academico/modulos/inscripcion/inscripcion.controller.ts
+++ src/academico/modulos/inscripcion/inscripcion.controller.ts
@@ -7,7 +7,7 @@ import {
   Post,
   Put,
 } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { InscripcionService } from "./inscripcion.service";
 import { CreateInscriptionDto } from "./dto/createInscription.dto";
 import { CreateMatriculaDto } from "./dto/createMatricula.dto";
@@ -17,18 +17,28 @@ import { CreateMatriculaDto } from "./dto/createMatricula.dto";
 export class InscripcionController {
   constructor(private readonly inscripcionService: InscripcionService) {}
 
+  @ApiOperation({
+    summary: "Crea Inscripcion",
+  })
   @Post()
   async createInscription(@Body() dto: CreateInscriptionDto) {
     const res = await this.inscripcionService.createInscription(dto);
     return res;
   }
 
+  @ApiOperation({
+    summary: "Crea Matricula",
+  })
   @Post("/matricula")
   async create(@Body() dto: CreateMatriculaDto) {
     const res = await this.inscripcionService.createMatricula(dto);
     return res;
   }
 
+  @ApiOperation({
+    summary:
+      "Devuelve todos los matriculados en una gestion, periodo, carrera e institucion educativa",
+  })
   @Get("/matriculados/:gestionId/:periodoId/:carreraId/:ieId")
   async getAllMatriculadosByGestion(
     @Param("gestionId") gestionId: string,
@@ -37,6 +47,27 @@ export class InscripcionController {
     @Param("ieId") ieId: string
   ) {
     return await this.inscripcionService.getAllMatriculadosByGestion(
+      parseInt(gestionId),
+      parseInt(periodoId),
+      parseInt(carreraId),
+      parseInt(ieId)
+    );
+  }
+
+  @ApiOperation({
+    summary:
+      "Devuelve todos las materias de una persona inscrita en una gestion, periodo, carrera e institucion educativa",
+  })
+  @Get("/detalle/:personaId/:gestionId/:periodoId/:carreraId/:ieId")
+  async getAllDetalleInscripcionPersona(
+    @Param("personaId") personaId: string,
+    @Param("gestionId") gestionId: string,
+    @Param("periodoId") periodoId: string,
+    @Param("carreraId") carreraId: string,
+    @Param("ieId") ieId: string
+  ) {
+    return await this.inscripcionService.getAllDetalleInscripcionPersona(
+      parseInt(personaId),
       parseInt(gestionId),
       parseInt(periodoId),
       parseInt(carreraId),
