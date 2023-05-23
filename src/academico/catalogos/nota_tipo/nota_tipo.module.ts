0877ee200dd183196d12dd5019442aca29bb91b1
cristinavallejos <cvallejos@minedu.gob.bo>
2023-05-23 18:28:24 -0400

    ajuste modulo de InscripcionCalificacion

--- src/academico/catalogos/nota_tipo/nota_tipo.module.ts
+++ src/academico/catalogos/nota_tipo/nota_tipo.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotaTipo } from 'src/academico/entidades/notaTipo.entity';
+import { DatabaseModule } from 'src/database/database.module';
+import { RespuestaSigedService } from 'src/shared/respuesta.service';
 import { NotaTipoController } from './nota_tipo.controller';
 import { NotaTipoService } from './nota_tipo.service';
 
 @Module({
+  imports:[
+    DatabaseModule, TypeOrmModule.forFeature([
+     NotaTipo      
+        ]),
+    ],
   controllers: [NotaTipoController],
-  providers: [NotaTipoService]
+  providers: [NotaTipoService, RespuestaSigedService]
 })
 export class NotaTipoModule {}
--- src/academico/modulos/instituto_estudiante_inscripcion_docente_calificacion/instituto_estudiante_inscripcion_docente_calificacion.module.ts
+++ src/academico/modulos/instituto_estudiante_inscripcion_docente_calificacion/instituto_estudiante_inscripcion_docente_calificacion.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { InstitutoEstudianteInscripcionDocenteCalificacion } from 'src/academico/entidades/institutoEstudianteInscripcionDocenteCalificacion.entity';
+import { DatabaseModule } from 'src/database/database.module';
 import { InstitutoEstudianteInscripcionDocenteCalificacionController } from './instituto_estudiante_inscripcion_docente_calificacion.controller';
 import { InstitutoEstudianteInscripcionDocenteCalificacionService } from './instituto_estudiante_inscripcion_docente_calificacion.service';
 
 @Module({
+  imports:[
+    DatabaseModule, TypeOrmModule.forFeature([
+     InstitutoEstudianteInscripcionDocenteCalificacion      
+        ]),
+    ],
   controllers: [InstitutoEstudianteInscripcionDocenteCalificacionController],
   providers: [InstitutoEstudianteInscripcionDocenteCalificacionService]
 })
